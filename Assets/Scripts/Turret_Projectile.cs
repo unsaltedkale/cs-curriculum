48768c230af73bfd9e5efacae49134cb9c3d48f2
unsaltedkale <abuckley0278@student.pps.net>
2024-12-17 13:04:51 -0800

    Update Turret_Projectile.cs

--- Assets/Scripts/Turret_Projectile.cs
+++ Assets/Scripts/Turret_Projectile.cs
@@ -11,6 +11,7 @@ public class Turret_Projectile : MonoBehaviour
     private GameManager gm;
     public Vector3 moveVector;
     public GameObject player;
+    private bool reachedTarget;
     
     void Start()
     {
@@ -22,7 +23,19 @@ public class Turret_Projectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, moveVector * 5, speed * Time.deltaTime);
+        if (!reachedTarget)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        }
+
+        if (Vector2.Distance(transform.position, target) > 1f)
+        {
+            Vector3 vector = target - transform.position;
+            Vector3.Normalize(vector);
+            print (vector);
+            Debug.DrawRay(vector);
+        }
+        
     }
     
     private void OnCollisionEnter2D(Collision2D other)
